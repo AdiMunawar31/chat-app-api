01e0b9f3bbbd78eeb866413940679953a1705238
adimunawar31 <munawar.adi31@gmail.com>
2024-10-14 23:41:26 +0700

    awokwowko

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const app_1 = require("./app");
-const PORT = process.env.PORT || 5000;
-app_1.httpServer.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
