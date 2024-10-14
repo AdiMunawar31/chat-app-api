0092f79ebec27f27d748ce3bafac140e8a77470e
adimunawar31 <munawar.adi31@gmail.com>
2024-10-14 23:23:04 +0700

    feat: add participants

--- api/models/room.model.ts
+++ api/models/room.model.ts
@@ -2,10 +2,12 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IRoom extends Document {
   name: string;
+  participants: string[];
 }
 
 const RoomSchema = new Schema<IRoom>({
   name: { type: String, required: true, unique: true },
+  participants: [{ type: String, required: true }],
 });
 
 export const Room = mongoose.model<IRoom>('Room', RoomSchema);
